ca94518fca406883dcd138a795f357fcc8ed90d1
L0ngshanks <lee.paulison.jr@gmail.com>
2023-11-17 17:57:25 -0500

    it begins

---
+++ src/layout/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// css
+import '../assets/css/App.css';
+// components
+import InteractiveMediaPlayer from './InteractiveMediaPlayer';
+
+function App() {
+  return (
+    <>
+      <InteractiveMediaPlayer />
+    </>
+  );
+}
+
+export default App;
