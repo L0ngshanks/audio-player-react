0a5fff6a40fcb3fe4189d99ad8945c1900cfbcc3
L0ngshanks <lee.paulison.jr@gmail.com>
2023-11-16 22:28:27 -0500

    redux installed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+// css
+import './assets/css/App.css';
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, incrementByAmount } from './slices/counterSlice';
 
 function App() {
-  return <div className="App">App</div>;
+  const count = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  return (
+    <div>
+      <div>
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+        <input
+          value={incrementAmount}
+          onChange={(evt) => setIncrementAmount(evt.target.value)}
+        />
+        <button
+          aria-label="Increment value by amount"
+          onClick={() => dispatch(incrementByAmount(incrementAmount))}
+        >
+          Increment by Amount
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
