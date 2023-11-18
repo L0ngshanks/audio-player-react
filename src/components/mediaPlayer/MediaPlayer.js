8f0649dfcdcc261ee9d45356c12035f1c86c7bf1
L0ngshanks <lee.paulison.jr@gmail.com>
2023-11-17 19:59:34 -0500

    fixing editor-prettier conflicts

--- src/components/mediaPlayer/MediaPlayer.js
+++ src/components/mediaPlayer/MediaPlayer.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 // redux
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 function MediaPlayer() {
-  console.log(useSelector((state) => state.media));
   const path = useSelector((state) => state.media.path);
   const media = useSelector((state) => state.media.media);
-  console.log(path);
+
   return (
     <div className="p-3">
       <h1>Media Player</h1>
       <audio controls>
         <source src={`${path}${media[1].src}`} type="audio/mpeg" />
       </audio>
+      <h1>Video</h1>
+      <video controls>
+        <source src={`${path}${media[0].src}`} type="video/mp4" />
+      </video>
     </div>
   );
 }
