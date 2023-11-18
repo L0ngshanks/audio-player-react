3c190266681d69a72c9cccda97e5305b4a651ee6
L0ngshanks <lee.paulison.jr@gmail.com>
2023-11-18 18:18:29 -0500

    setup for testing

--- src/components/mediaPlayer/MediaPlayer.js
+++ src/components/mediaPlayer/MediaPlayer.js
@@ -7,14 +7,14 @@ function MediaPlayer() {
   const media = useSelector((state) => state.media.media);
 
   return (
-    <div className="p-3">
+    <div className='p-3'>
       <h1>Media Player</h1>
       <audio controls>
-        <source src={`${path}${media[1].src}`} type="audio/mpeg" />
+        <source src={`${path}${media[1].src}`} type='audio/mpeg' />
       </audio>
       <h1>Video</h1>
-      <video controls>
-        <source src={`${path}${media[0].src}`} type="video/mp4" />
+      <video controls width='640px' height='360px'>
+        <source src={`${path}${media[0].src}`} type='video/mp4' />
       </video>
     </div>
   );
